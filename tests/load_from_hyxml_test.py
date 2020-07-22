c9695fc00d9ac48772d4972d56319b0117024784
Zongnan Bao <nick19981122@163.com>
2020-07-22 17:10:07 -0500

    testfile

---
+++ tests/load_from_hyxml_test.py
@@ -0,0 +1,15 @@
+import os
+from c2e2 import FileHandler
+
+# Test load model
+TOTALV2 = os.path.dirname(os.path.abspath(__file__)) + "/../TotalMotionV2.hyxml"
+
+print("=== TEST Begin ===\n")
+print("--- Testing FileHandler.load_model(filename) ---")
+
+automata, properties = FileHandler.load_model(TOTALV2)
+print(automata)
+print(properties)
+
+
+print("=== End of TEST ===")
